687b6d18d96cb2963a27d07cd2e178e49d3add9d
asthashaw <astha.shaw100@gmail.com>
2024-09-02 14:53:37 +0100

    Add intro to javaScript files for Day 1 workshop

---
+++ largestNumber.js
@@ -0,0 +1,62 @@
+const a=26, b=12, c=10;
+//a=45;
+let largestNumber=c;
+
+if(a>b && a>c){
+    largestNumber=a;
+}
+else if(b>a && b>c){
+    largestNumber=b;
+}
+
+console.log(`The largest number is ::${largestNumber}`)
+
+
+//2nd way of computing largestNumber using functions-DRY
+
+let largestValue=computeLargestNumber(400,600,800);
+console.log(`The largest number is ::${largestValue}`)
+function computeLargestNumber(firstNumber, secondNumber, thirdNumber){
+    let largestNumber=firstNumber;
+    firstNumber=100;
+    if(secondNumber>firstNumber && secondNumber>thirdNumber){
+        largestNumber=secondNumber;
+    }
+    else if(thirdNumber>secondNumber && thirdNumber>firstNumber){
+        largestNumber=thirdNumber;
+    }
+    
+    return largestNumber;
+}
+
+//write functions using arrows
+const largestArrowWay=computeLargestNumberArrowWay(20,10,560)
+const computeLargestNumberArrowWay=(firstNumber, secondNumber,thirdNumber)=>{
+
+    let largestNumber=firstNumber;
+    //firstNumber=100;
+    if(secondNumber>firstNumber && secondNumber>thirdNumber){
+        largestNumber=secondNumber;
+    }
+    else if(thirdNumber>secondNumber && thirdNumber>firstNumber){
+        largestNumber=thirdNumber;
+    }
+    
+    return largestNumber;
+}
+
+//ternary way of writing
+
+const ternaryWayComputeLargestNumber=(a,b,c)=>{
+    return (a>b && a>c)?a:((b>a && b>c)?b:c);
+}
+const largest=ternaryWayComputeLargestNumber(50,60,70)
+
+console.log(`The largest number is ::${largest}`)
+
+
+//You need to fizzBuzzCalculator(elements)
+//15--->1,2,Fizz,4,Buzz,Fizz..........FizzBuzz
+
+//how do you compute whether a number is divisible by another number
+(number%3==0)
